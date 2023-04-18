da332228f99eb74f518e6f8d8cbe471675e4c9fc
Alfredo Barral <alfredo.barral@smallworldfs.com>
2023-04-18 10:11:12 +0200

    commit 2

--- src/main/java/com/smallworldfs/transactionservice/transaction/api/TransactionController.java
+++ src/main/java/com/smallworldfs/transactionservice/transaction/api/TransactionController.java
@@ -1,7 +1,10 @@
 package com.smallworldfs.transactionservice.transaction.api;
 
+import com.smallworldfs.transactionservice.transaction.api.mapper.TransactionDtoMapper;
+import com.smallworldfs.transactionservice.transaction.api.model.TransactionDto;
 import com.smallworldfs.transactionservice.transaction.service.TransactionService;
 import lombok.RequiredArgsConstructor;
+import org.mapstruct.factory.Mappers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,11 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/transactions")
 public class TransactionController {
 
+    private final TransactionDtoMapper mapper = Mappers.getMapper(TransactionDtoMapper.class);
     private final TransactionService service;
 
     @GetMapping("/{id}")
-    public void getTransaction(@PathVariable Integer id) {
-        service.getTransaction(id);
+    public TransactionDto getTransaction(@PathVariable Integer id) {
+        return mapper.toDto(service.getTransaction(id));
     }
 
 }
