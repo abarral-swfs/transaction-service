e42a481001f9582430e2672b35d85b0f1b8e48f6
Alfredo Barral <alfredo.barral@smallworldfs.com>
2023-04-18 11:47:00 +0200

    TransactionDataServiceClientTest

---
+++ src/test/java/com/smallworldfs/transactionservice/TransactionServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.smallworldfs.transactionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(properties = "transaction-data-service.url=http://localhost:8080")
+class TransactionServiceApplicationTests {
+
+    // @MockBean
+    // private TransactionDataServiceClient client;
+
+    @Test
+    void contextLoads() {}
+
+}
